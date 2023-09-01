a3d55526a1b6f332821016b3927250e02c26b2ab
Bhabishya Gurung <bhab_ej@hotmail.com>
2023-09-01 14:14:30 +0300

    calculator added

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
-  const [text, setText] = useState("");
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState("");
 
-  const buttonPressed = () => {
-    Alert.alert('Hello', 'You typed: ' + text);
+  const addition = () => {
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(sum.toString());
+  }
+
+  const subtraction = () => {
+    const substract = parseFloat(num1) - parseFloat(num2);
+    setResult(substract.toString());
   }
 
   return (
-    <View style={styles.container}>
-      <TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}}
-      onChangeText={text => setText(text)} value={text} />
-      <Button title='Press me' onPress={buttonPressed}></Button>
-      <StatusBar style='auto' />
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      {result !== "" && <Text style={{ fontSize: 25, marginTop: 25}}>Result: {result}</Text>}
+      <TextInput
+        style={{borderWidth: 1, padding: 10, marginBottom: 10, width: 200}}
+        keyboardType = "numeric"
+        value={num1}
+        onChangeText={setNum1}
+      />
+      <TextInput 
+        style={{borderWidth: 1, padding: 10, marginBottom: 10, width: 200}}
+        keyboardType = "numeric"
+        value={num2}
+        onChangeText={setNum2}
+      />
+      <Button 
+        title="+"
+        onPress={addition}
+      />
+      <Button 
+        title="-"
+        onPress={subtraction}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
